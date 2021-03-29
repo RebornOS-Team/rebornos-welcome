3a769db09b775199bc29bb79f80eca85a53968ce
shivanandvp <shivanand.pattanshetti@gmail.com>
2021-03-29 17:12:38 -0500

    .

--- scripts/build-archlinux.sh
+++ scripts/build-archlinux.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env sh
 
 (# Go into a subshell to nullify directory changes
-    cd packaging/archlinux && makepkg -f --noextract
+    cd packaging/archlinux \
+    && makepkg -f --noextract \
+    && sudo pacman -U --noconfirm *.pkg.tar.zst \
+    && gpg --detach-sign --use-agent *.pkg.tar.zst
 )
