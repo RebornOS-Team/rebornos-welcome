d263e195744120756a3fd489c2a8fb2a9f983b17
shivanandvp <shivanandvp.oss@gmail.com>
2023-08-14 00:59:37 -0500

    build: :hammer: Revamp build scripts

--- scripts/rebuild_for_archlinux-iso.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env sh
-
-(# Go into a subshell to nullify directory changes
-    cd packaging/archlinux-iso \
-    && makepkg --cleanbuild --noextract --clean --force --syncdeps "$@" #\
-    # && gpg --detach-sign --use-agent *.pkg.tar.zst
-)
--- scripts/rebuild_for_archlinux.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env sh
-
-(# Go into a subshell to nullify directory changes
-    cd packaging/archlinux \
-    && makepkg --cleanbuild --noextract --clean --force --syncdeps "$@" #\
-    # && gpg --detach-sign --use-agent *.pkg.tar.zst
-)
-
